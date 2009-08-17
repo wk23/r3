2df05348640cb01f6a715a212e6f15340adab139
unknown <Administrator@.(none)>
2009-08-17 18:05:19 +0300

    revert dualspec

--- src/game/SharedDefines.h
+++ src/game/SharedDefines.h
@@ -442,10 +442,6 @@ enum ItemQualities
 #define SPELL_ATTR_EX6_UNK30                      0x40000000            // 30 not set in 3.0.3
 #define SPELL_ATTR_EX6_UNK31                      0x80000000            // 31 not set in 3.0.3
 
-#define MIN_TALENT_SPEC         0
-#define MAX_TALENT_SPEC         1
-#define MIN_TALENT_SPECS        1
-#define MAX_TALENT_SPECS        2
 #define MAX_GLYPH_SLOT_INDEX    6
 
 enum SheathTypes
