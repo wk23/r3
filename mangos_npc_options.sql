aaa60dc3dd31ca36610d01b1672d2fe5497f01fb
unknown <Administrator@.(none)>
2009-08-19 13:48:58 +0300

    dualspec

---
+++ mangos_npc_options.sql
@@ -0,0 +1,1 @@
+INSERT INTO `npc_option` (`id`, `gossip_id`, `npcflag`, `icon`, `action`, `box_money`, `coded`, `option_text`, `box_text`) values('52','0','16','2','19','10000000','0','Purchase a Dual Talent Specialization.','Are you sure you wish to purchase a Dual Talent Specialization?');
